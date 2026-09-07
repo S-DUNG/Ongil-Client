c516869af73e8ebcc0dac3a6a50807230253afca
선가영 <s26036@gsm.hs.kr>
2026-09-08 07:56:35 +0900

    refactor: 도움 요청 페이지 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+// src/App.tsx
 import BusInfoPage from './pages/BusInfoPage'
+
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-[#F7F7F5]">
+      {/* 🌟 도움 요청, 홈 버튼 다 날리고 깔끔하게 버스 정보 페이지만 렌더링! */}
       <BusInfoPage />
     </div>
   )
